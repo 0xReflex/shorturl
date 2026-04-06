2ecf877e58a8ff625b19d5dfcaeb9916744f7edd
0xReflex <58839586+0xReflex@users.noreply.github.com>
2026-04-07 04:21:05 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,37 +1,15 @@
 const mongoose = require('mongoose');
 
-let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+async function connectToMongoDB(url){
+    try{
+        await mongoose.connect(url);
+        return console.log('connected to database');
+    }catch(err){
+        console.log('Error', err);
+    }
 }
 
-async function connectToMongoDB(url) {
-  if (cached.conn) {
-    // Return cached connection if already connected
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // optional: fail fast if DB not reachable
-    };
-
-    cached.promise = mongoose.connect(url, opts).then((mongooseInstance) => {
-      return mongooseInstance;
-    });
-  }
-
-  try {
-    cached.conn = await cached.promise;
-    console.log('Connected to MongoDB');
-    return cached.conn;
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    throw err;
-  }
+module.exports = {
+    connectToMongoDB,
 }
-
-module.exports = { connectToMongoDB };
